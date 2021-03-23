5777589cf89297bae206d1ee4b01ac0c1d2dca23
Lucia <lgarcia@eepmad.com>
2021-03-23 13:16:58 +0100

    hol

---
+++ src/main/java/com/mycompany/labo/SecondaryController.java
@@ -0,0 +1,33 @@
+package com.mycompany.labo;
+
+import java.io.IOException;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+
+
+public class SecondaryController {
+
+    private ObservableList<Usuario> lista = FXCollections.observableArrayList();
+
+    @FXML
+    private ListView listaUsuarios;
+
+    @FXML
+    private void addUsuario() {
+        lista.add(new Usuario("Usuario nombre", "123"));
+    }
+
+    @FXML
+    private void switchToPrimary() throws IOException {
+        App.setRoot("primary");
+    }
+
+    public void inicializaListados() {
+        lista.add(new Usuario("Sergio", "sergio45"));
+        listaUsuarios.setItems(lista);
+
+        listaUsuarios.setItems(lista);
+}
+}
